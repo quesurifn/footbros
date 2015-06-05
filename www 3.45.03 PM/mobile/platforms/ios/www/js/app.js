fd8bb03aefeed03636ec527debcdadef0f3e33d0
nonche <nonche@nonches-MacBook-Pro.local>
2015-06-05 15:49:48 -0500

    new shit

---
+++ www 3.45.03 PM/mobile/platforms/ios/www/js/app.js
@@ -0,0 +1,84 @@
+angular.module('explorer', ['ionic', 
+    'explorer.product',
+    'explorer.controllers',
+    'auth0',
+    'angular-storage',
+    'angular-jwt'])
+
+    .run(function ($ionicPlatform, $rootScope) {
+
+        $rootScope.server = "http://footbros.com";
+
+        $ionicPlatform.ready(function () {
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+              cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+              // org.apache.cordova.statusbar required
+              StatusBar.styleDefault();
+            }
+        });
+    })
+    .config(function ($sceDelegateProvider) {
+        $sceDelegateProvider.resourceUrlWhitelist(['self', new RegExp('^(http[s]?):\/\/(w{3}.)?youtube\.com/.+$')]);
+    })
+    .config(function ($stateProvider, $urlRouterProvider, authProvider,
+      jwtInterceptorProvider, $httpProvider) {
+
+        $stateProvider
+          .state('login', {
+            url: "/login",
+            templateUrl: "templates/login.html",
+            controller: 'LoginCtrl'
+          })
+          .state('app', {
+            url: "/products",
+            abstract: true,
+            templateUrl: "templates/menu.html",
+            controller: 'AppCtrl'
+          })
+        $urlRouterProvider.otherwise('/products');
+        // Configure Auth0
+          authProvider.init({
+            domain: AUTH0_DOMAIN,
+            clientID: AUTH0_CLIENT_ID,
+            loginState: 'products'
+          });
+
+        jwtInterceptorProvider.tokenGetter = function(store, jwtHelper, auth) {
+          var idToken = store.get('token');
+          var refreshToken = store.get('refreshToken');
+          if (!idToken || !refreshToken) {
+            return null;
+          }
+          if (jwtHelper.isTokenExpired(idToken)) {
+            return auth.refreshIdToken(refreshToken).then(function(idToken) {
+              store.set('token', idToken);
+              return idToken;
+            });
+          } else {
+            return idToken;
+          }
+        }
+
+        $httpProvider.interceptors.push('jwtInterceptor');
+    })
+    .run(function($rootScope, auth, store) {
+      $rootScope.$on('$locationChangeStart', function() {
+        if (!auth.isAuthenticated) {
+          var token = store.get('token');
+          if (token) {
+            auth.authenticate(store.get('profile'), token);
+          }
+        }
+
+      });
+
+    });
+
+
---
+++ www 3.45.03 PM/mobile/platforms/ios/www/js/controllers.js
@@ -0,0 +1,50 @@
+angular.module('explorer.controllers', [])
+.controller('LoginCtrl', function($scope, auth, $state, store) {
+  function doAuth() {
+    auth.signin({
+      closable: false,
+      // This asks for the refresh token
+      // So that the user never has to log in again
+      authParams: {
+        scope: 'openid offline_access'
+      }
+    }, function(profile, idToken, accessToken, state, refreshToken) {
+      store.set('profile', profile);
+      store.set('token', idToken);
+      store.set('refreshToken', refreshToken);
+      $state.go('tab.dash');
+    }, function(error) {
+      console.log("There was an error logging in", error);
+    });
+  }
+
+  $scope.$on('$ionic.reconnectScope', function() {
+    doAuth();
+  });
+
+  doAuth();
+})
+.controller('AppCtrl', function($scope) {
+  
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+  
+})
+.controller('DashCtrl', function($scope, $http) {
+  $scope.callApi = function() {
+    // Just call the API as you'd do using $http
+    $http({
+      url: 'http://auth0-nodejsapi-sample.herokuapp.com/secured/ping',
+      method: 'GET'
+    }).then(function() {
+      alert("We got the secured data successfully");
+    }, function() {
+      alert("Please download the API seed so that you can call it.");
+    });
+  };
+
+});
