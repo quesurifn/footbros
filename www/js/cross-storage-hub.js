ab7e46ca3792739f2dfa997f888a4d2124996af4
Jason Tanner <jasontanner328@gmail.com>
2015-06-12 12:23:46 -0400

    adding logs

--- www/js/cross-storage-hub.js
+++ www/js/cross-storage-hub.js
@@ -269,11 +269,11 @@
   }
 }(this));
 
-
+console.log("initiating cross storage hub")
 CrossStorageHub.init([
   {
     origin: /\.herokuapp.com$/,
-    allow: ["get"]
+    allow: ["get", "set"]
   }, {
     origin: /:\/\/(www\.)?herokuapp.com$/,
     allow: ["get", "set", "del"]
